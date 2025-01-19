d5b2d0cda8b7e09b2c3486caba186c2250cc53cb
Sathish Kumar <sathishkumar.p@kshema.co>
2025-01-19 12:22:33 +0530

    task-1

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,13 +1,33 @@
-import { List } from 'ui'
+// import { List } from 'ui'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
+interface MyPokiLists {
+  name: string;
+  url: string;
+}
+
 const App = () => {
+  const [pokeLists, setPokiLists] = useState<MyPokiLists[]>([])
+
+  // 1)How did you manage to fetch the list and what tool did you use?
+  // Ans: I have user Third party library called axios to fetch pokelists from API
+
+  // 2) What steps would you take to future improve this?
+  // Ans: Will create commmon component to fetch api responses and also will create interceptor for axios
+   
+
+
+  useEffect(() => {
+      axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then(res => {
+        setPokiLists(res.data.results)
+      })
+  },[])
+
   return (
-  <>
     <h1>Pokemon list:</h1>
-    <List />
-  </>
   )
 }
 
